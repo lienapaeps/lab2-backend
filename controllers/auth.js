2c63ecc3a16e69cd62161ee8f563b4781c9c7683
lienapaeps <paepsliena@gmail.com>
2023-05-01 15:56:11 +0200

    feat: create user via postman

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,31 @@
+const User = require('../models/user');
+const passport = require('passport');
+
 // user sign up 
+const signup = async (req, res) => {
+    console.log(req.body);
+
+    //momenteel testen via postman
+    let username = req.body.username;
+    let password = req.body.password;
+
+    const user = new User({username: username});
+    await user.setPassword(password);
+    await user.save().then(result => {
+        res.json({
+            "status": "success"
+        })
+    }).catch(error => {
+        res.json({
+            "status": "error"
+        })
+    });
+};
 
 // user login
 
 // changePassword
+
+module.exports = {
+    signup
+};
