60ea070c3367244d5d11b4b80531694ea738ee44
lienapaeps <paepsliena@gmail.com>
2023-05-02 19:49:17 +0200

    feat: add validation to login and register

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,27 @@ const signup = async (req, res) => {
     let firstname = req.body.firstname;
     let lastname = req.body.lastname;
 
-    User.register(new User({email: email, firstname: firstname, lastname: lastname}), password, function(err, user) {  
+    User.register(new User({email: email, firstname: firstname, lastname: lastname}), password, function(err, user) { 
+        // alle velden leeg
+        if (email === "" || password === "" || firstname === "" || lastname === "" || password === "") {
+            return res.json({
+                "status": "error",
+                "message": "Vul alle velden in"
+            })
+        // wachtwoord moet minstens 6 karakters zijn
+        } else if (password.length < 6) {
+            return res.json({
+                "status": "error",
+                "message": "Password moet langer zijn dan 6 karakters"
+            })
+        } 
+        // nakijken of email een geldig emailadres is
+        else if (!email.includes("@")) {
+            return res.json({
+                "status": "error",
+                "message": "Vul een geldig emailadres in"
+            })
+        }
         if (err) {
             console.log(err);
             res.json({
@@ -18,48 +38,37 @@ const signup = async (req, res) => {
         } else {
             res.json({
                 "status": "success",
-                "message": "User created",
+                "message": "Nieuwe gebruiker aangemaakt",
             })
         }
-    });    
-
-    // const user = new User({email: email});
-    // user.firstname = firstname;
-    // user.lastname = lastname;
-    // user.setPassword(password);
-    // await user.save().then(result => {
-    //     res.json({
-    //         "status": "success",
-    //         "message": "User created",
-    //     })
-    // }).catch(error => {
-    //     res.json({
-    //         "status": "error",
-    //         "message": "Er ging iets mis"
-    //     })
-    // });
+    });  
 };
 
 // user login
 const login = async (req, res) => {
     const user = await User.authenticate()(req.body.email, req.body.password).then(result => {
-        // no user found
-        if (!result.user) {
+        // email en wachtwoord leeg
+        if (req.body.email === "" || req.body.password === "") {
             return res.json({
                 "status": "error",
-                "message": "Invalid email or password"
+                "message": "Vul alle velden in"
+            })
+        // geen gebruiker gevonden of wachtwoord onjuist
+        } else if (!result.user) {
+            return res.json({
+                "status": "error",
+                "message": "E-mail of wachtwoord is onjuist"
             })
         }
-
-        // user found
+        // gebruiker gevonden en wachtwoord juist
         res.json({
             "status": "success",
-            "message": "User logged in",
+            "message": "Gebruiker is ingelogd",
         })
     }).catch(error => {
         res.json({
             "status": "error",
-            "message": error
+            "message": "Er ging iets mis"
         })
     });
 }
