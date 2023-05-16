fe0274a00c3af56e8b3394c0dc24bf991421b60a
lienapaeps <paepsliena@gmail.com>
2023-05-16 12:43:55 +0200

    feat: add user to farm schema

--- models/farm.js
+++ models/farm.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const farmSchema = new Schema ({
+    user: String,
     name: {
         type: String,
         required: true
